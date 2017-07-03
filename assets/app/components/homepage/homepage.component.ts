0c5ed274d78e902f97cb235770dfc38139aff68f
Sherin Binu <sherinbinu@hotmail.com>
2017-07-03 15:52:04 +0530

    parallax scrolling effect initial fix

--- assets/app/components/homepage/homepage.component.ts
+++ assets/app/components/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit} from '@angular/core';
+import 'jquery';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
@@ -13,5 +14,15 @@ export class HomePageComponent implements OnInit{
   }
 
   ngOnInit() {
+    $(document).ready(function() {
+        $('section[data-type="background"]').each(function () {
+            var $bgobj = $(this);
+            $(window).scroll(function () {
+                var yPos = -($(window).scrollTop() / $bgobj.data('speed'));
+                var coords = '50% ' + yPos + 'px';
+                $bgobj.css({backgroundPosition: coords});
+            });
+        });
+    });
   }
 }
