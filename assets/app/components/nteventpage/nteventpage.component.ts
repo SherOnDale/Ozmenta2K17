e4b1e5d7517c2f56c30ddff4d41a39050430a836
Sherin Binu <sherinbinu@hotmail.com>
2017-07-19 11:29:05 +0530

    event registration implemented for all events

--- assets/app/components/nteventpage/nteventpage.component.ts
+++ assets/app/components/nteventpage/nteventpage.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {MdDialog, MdDialogConfig} from '@angular/material';
+import {MdDialog, MdDialogConfig, MdSnackBar} from '@angular/material';
 import {MoreService} from '../../services/more.service';
+import {UserService} from '../../services/user.service';
 import {More} from '../../models/more.model';
 import {MoreComponent} from '../more/more.component';
 import {RegisterEventComponent} from '../registerevent/registerevent.component';
@@ -18,10 +19,27 @@ export class NTEventPageComponent implements OnInit {
     this.data = this.moreService.getNMore();
   }
 
-    constructor(private moreService: MoreService, private dialog: MdDialog) {}
+    constructor(private moreService: MoreService, private dialog: MdDialog, private userService: UserService, private snackBar: MdSnackBar) {}
 
-    openConfirmation() {
+    openConfirmation(eventId) {
       const dialogRef = this.dialog.open(RegisterEventComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      if(result == "confirm") {
+        const body = {
+          eventId: eventId,
+          token: this.userService.getToken()
+        }
+        this.userService.registerEvent(body)
+          .subscribe(
+            data => {
+              this.snackBar.open('Successfully Registerd', 'OK');
+            }, 
+            error => {
+              this.snackBar.open('Registration Faled. Please Try Again Later', 'OK');
+            }
+          );
+      }
+    });
     }
 
   openDialog(data) {
--- assets/app/components/teventpage/teventpage.component.ts
+++ assets/app/components/teventpage/teventpage.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {MdDialog, MdDialogConfig} from '@angular/material';
+import {MdDialog, MdDialogConfig, MdSnackBar} from '@angular/material';
 import {MoreService} from '../../services/more.service';
+import {UserService} from '../../services/user.service';
 import {More} from '../../models/more.model';
 import {MoreComponent} from '../more/more.component';
 import {RegisterEventComponent} from '../registerevent/registerevent.component';
@@ -18,7 +19,7 @@ export class TEventPageComponent implements OnInit {
     this.data = this.moreService.getTMore();
   }
 
-    constructor(private moreService: MoreService, private dialog: MdDialog) {}
+    constructor(private moreService: MoreService, private dialog: MdDialog, private userService: UserService, private snackBar: MdSnackBar) {}
 
   openDialog(data) {
     const config = new MdDialogConfig();
@@ -26,7 +27,24 @@ export class TEventPageComponent implements OnInit {
     const dialogRef = this.dialog.open(MoreComponent, config);
   }
 
-  openConfirmation() {
+  openConfirmation(eventId) {
     const dialogRef = this.dialog.open(RegisterEventComponent);
+    dialogRef.afterClosed().subscribe(result => {
+      if(result == "confirm") {
+        const body = {
+          eventId: eventId,
+          token: this.userService.getToken()
+        }
+        this.userService.registerEvent(body)
+          .subscribe(
+            data => {
+              this.snackBar.open('Successfully Registerd', 'OK');
+            }, 
+            error => {
+              this.snackBar.open('Registration Faled. Please Try Again Later', 'OK');
+            }
+          );
+      }
+    });
   }
 }
